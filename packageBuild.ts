fbfc1c19353e33c342e861d6b3ced9c5e05ae2b5
dirtbikercj <62989449+dirtbikercj@users.noreply.github.com>
2023-09-03 08:05:35 -0400

    Update packageBuild.ts

--- packageBuild.ts
+++ packageBuild.ts
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
-
 /**
- * 
+ *
  * Credits: Dirtbikercj
- * 
+ *
  */
 
 const fs = require("fs-extra");
 const glob = require("glob");
 const zip = require("bestzip");
 const path = require("path");
-const minimatch = require("minimatch");
 const chalk = require("chalk");
 const processHost = require("child_process")
 
@@ -28,165 +26,170 @@ const epoch = `${currentDate}`
 console.log(`[SPT build System] ${epoch}`);
 if (config.GitStatus === true)
 {
-    const gitStatus = processHost.execSync("git status -uno", {encoding: "utf-8"})
-    console.log(gitStatus);
+    if (fs.existsSync(`${currentDir}/.git`))
+    {
+        const gitStatus = processHost.execSync("git status -uno", {encoding: "utf-8"})
+        console.log(gitStatus);
+    }
+    else
+        console.log(chalk.yellow("[SPT Build System] GitStatus is enabled in build.json, but this isn't a Git repository."));
 }
-console.log("[SPT Build System] Build options:");
-console.log(`[SPT Build System] CopyToGame:     ${config.CopyToGame}`);
-console.log(`[SPT Build System] PathToRoot:     ${config.PathToRoot}`);
-console.log(`[SPT Build System] BuildDir:       ${config.BuildDir}`);
-console.log(`[SPT Build System] CopyBundles:    ${config.CopyBundles}`);
-console.log(`[SPT Build System] BuildZip:       ${config.BuildZip}`);
-console.log(`[SPT Build System] StartServer:    ${config.StartServer}`);
-console.log(`[SPT Build System] StartDelay:     ${config.StartDelay}`);
+if (config.ShowBuildOptions)
+{
+    console.log("[SPT Build System] Build options:");
+    console.log(`[SPT Build System] ContainPlugin:  ${config.ContainPlugin}`);
+    console.log(`[SPT Build System] CopyBundles:    ${config.CopyBundles}`);
+    console.log(`[SPT Build System] BuildZip:       ${config.BuildZip}`);
+    if (config.ContainPlugin)
+    {
+        console.log(config.PluginFileName !== "" ?`[SPT Build System] PluginFileName: ${config.PluginFileName}`:chalk.red("[SPT Build System] PluginFileName: Not configured."));
+        console.log(config.PluginPath !== "" ?`[SPT Build System] PluginPath:     ${config.PluginPath}`:chalk.red("[SPT Build System] PluginPath:     Not configured."));
+    }
+    console.log(`[SPT Build System] BuildDir:       ${config.BuildDir}`);
+    console.log(`[SPT Build System] CopyToGame:     ${config.CopyToGame}`);
+    console.log(`[SPT Build System] PathToGame:     ${config.PathToGame}`);
+    console.log(`[SPT Build System] StartServer:    ${config.StartServer}`);
+    console.log(`[SPT Build System] StartDelay:     ${config.StartDelay}`);
+}
 console.log(`[SPT Build System] Generated package name: ${modName}`);
 
 if (config.FirstRun)
 {
     // First run on new build system, remove dist folder.
     fs.rmSync(`${currentDir}\\dist`, { force: true, recursive: true });
-    console.log(chalk.yellow("[SPT Build System] First time running npm build"));
-    console.log(chalk.yellow("[SPT Build System] Please configure the build.json file"));
-    console.log(chalk.yellow("[SPT Build System] Disable this warning by disabling FirstRun in build.json"));
+    console.log(chalk.yellow("[SPT Build System] First time running npm build."));
+    console.log(chalk.yellow("[SPT Build System] Please configure the build.json file."));
+    console.log(chalk.yellow("[SPT Build System] Disable this warning by disabling FirstRun in \"build.json\"."));
     process.exit(1);
 }
 
+
 fs.rmSync(`${currentDir}\\${config.BuildDir}`, { force: true, recursive: true });
+fs.rmSync(`${currentDir}\\${config.BuildDir}_zip`, { force: true, recursive: true });
 console.log(chalk.green("[SPT Build System] Previous build files deleted."));
-
-const ignoreList = [
-    "node_modules/",
-    // "node_modules/!(weighted|glob)", // Instead of excluding the entire node_modules directory, allow two node modules.
-    //"src/**/*.js",
-    "types/",
-    ".git/",
-    ".gitea/",
-    ".eslintignore",
-    ".eslintrc.json",
-    ".gitignore",
-    ".DS_Store",
-    "packageBuild.ts",
-    "mod.code-workspace",
-    "package-lock.json",
-    "tsconfig.json",
-    "build.json"
-];
-
-const ignoreListNoBundles = [
-    "bundles/",
-    "node_modules/",
-    // "node_modules/!(weighted|glob)", // Instead of excluding the entire node_modules directory, allow two node modules.
-    //"src/**/*.js",
-    "types/",
-    ".git/",
-    ".gitea/",
-    ".eslintignore",
-    ".eslintrc.json",
-    ".gitignore",
-    ".DS_Store",
-    "packageBuild.ts",
-    "mod.code-workspace",
-    "package-lock.json",
-    "tsconfig.json",
-    "build.json"
-];
 
 let exclude;
 if (config.CopyBundles === true)
 {
-    exclude = glob.sync(`{${ignoreList.join(",")}}`, { realpath: true, dot: true });
+    exclude = glob.sync(`{${config.IgnoreList.join(",")}}`, { realpath: true, dot: true });
 }
 else
 {
-    exclude = glob.sync(`{${ignoreListNoBundles.join(",")}}`, { realpath: true, dot: true });
+    config.IgnoreList.push("bundles/")
+    exclude = glob.sync(`{${config.IgnoreList.join(",")}}`, { realpath: true, dot: true });
 }
 
 fs.copySync(currentDir, path.normalize(`${currentDir}/../~${modName}`), {filter: (src) =>
-{
-    const relativePath = path.relative(currentDir, src);
-    const shouldExclude = exclude.some((pattern) => minimatch(relativePath, pattern));
-    return !shouldExclude;
-}});
+    {
+        return !exclude.includes(src);
+    }});
 
-fs.moveSync(path.normalize(`${currentDir}/../~${modName}`), path.normalize(`${currentDir}/${modName}`), { overwrite: true });
-fs.copySync(path.normalize(`${currentDir}/${modName}`), path.normalize(`${currentDir}/${config.BuildDir}`));
-
-if (config.BuildZip === false)
+//temp folder path (only used with BuildZip enabled in build.json)
+const tempPath = path.normalize(`${currentDir}/temp`)
+const serverModPath = path.normalize(`${currentDir}/${config.BuildDir}/user/mods/${modName}`)
+if (config.BuildZip)
 {
-    // If we are not building the zip, remove the tmp directory
-    fs.rmSync(`${currentDir}/${modName}`, { force: true, recursive: true });
+    //Creating a temp folder with the mod
+    fs.moveSync(path.normalize(`${currentDir}/../~${modName}`), tempPath, {overwrite: true});
+    //Copying temp folder to output (build) folder with the proper structure
+    fs.copySync(tempPath, serverModPath);
+}
+else
+{
+    //No need to create a temp folder
+    fs.moveSync(path.normalize(`${currentDir}/../~${modName}`), serverModPath, {overwrite: true});
 }
 
-console.log(chalk.green(`[SPT Build System] Server files built. ${config.BuildDir} directory contains your newly built files`));
+
+if (config.ContainPlugin)
+{
+    const pluginFolder = config.PluginPath !== ""?path.normalize(`${config.PluginPath}`):"Not configured.";
+    if (fs.existsSync(pluginFolder) && config.PluginPath !== "")
+        fs.copySync(path.normalize(`${pluginFolder}/${config.PluginFileName}`), path.normalize(`${currentDir}/${config.BuildDir}/BepInEx/plugins/${config.PluginFileName}`));
+    else
+        console.log(chalk.red(`[SPT Build System] Couldn't find your configured plugin folder: ${pluginFolder}`));
+}
+
+console.log(chalk.green(`[SPT Build System] Server files built. ${config.BuildDir} directory contains your newly built files.`));
 
 if (config.CopyToGame)
 {
-    // Delete the existing build before we copy the new build over.
-    fs.rmSync(`${config.PathToRoot}/user/mods/${modName}`, { force: true, recursive: true });
-    console.log(chalk.green("[SPT Build System] Server files deleted in game directory."));
+    //const gamePath = path.normalize(`${config.PathToGame}`)
+    if (fs.existsSync(config.PathToGame))
+    {
+        // Delete the existing build before we copy the new build over.
+        fs.rmSync(`${config.PathToGame}/user/mods/${modName}`, {force: true, recursive: true});
+        console.log(chalk.green("[SPT Build System] Files deleted in game directory."));
 
-    const gamePath = path.normalize(`${config.PathToRoot}/user/mods/${modName}`)
-
-    // Copy files to the game directory
-    fs.copySync(path.normalize(`${currentDir}/${config.BuildDir}`), gamePath);
-    console.log(chalk.green(`[SPT Build System] Server files copied to game path: ${gamePath}`));
+        // Copy files to the game directory
+        fs.copySync(path.normalize(`${currentDir}/${config.BuildDir}`), config.PathToGame);
+        console.log(chalk.green(`[SPT Build System] Files copied to game path: "${config.PathToGame}".`));
+    }
+    else
+        console.log(chalk.red(`[SPT Build System] Copy failed, couldn't find your game folder at: "${config.PathToGame}".`));
 }
 
 if (config.BuildZip)
 {
-    // const representing the root of the drive for the executing process.
-    const rootPathC = path.parse(process.cwd()).root;
-
     // Create the temporary user/mod path
-    const relativeZipPath = path.normalize(path.join(`${rootPathC}/`, "user/", "mods/"));
-    fs.mkdir(relativeZipPath, { recursive: true }, (err) => 
+    const relativeZipPath = path.normalize(path.join(`${tempPath}/`, "user/", "mods/"));
+    fs.mkdir(relativeZipPath, { recursive: true }, (err) =>
     {
-        if (err) 
+        if (err)
         {
-            console.error(chalk.red(`[SPT Build System] A error has occurred creating directory ${relativePath}: `, err.stack), err);
+            console.error(chalk.red(`[SPT Build System] A error has occurred creating directory "${relativeZipPath}": `, err.stack), err);
         }
     });
-    
-    // copy the temp build directory into the temporary user/mod directory
-    fs.copySync(path.normalize(`${currentDir}/${modName}`), path.normalize(`${relativeZipPath}/${modName}`));
+
+    if (!fs.existsSync(path.normalize(`${currentDir}\\${config.BuildDir}_zip`)))
+        fs.mkdirSync(path.normalize(`${currentDir}\\${config.BuildDir}_zip`));
 
     zip({
-        source: `${relativeZipPath}\\${modName}\\`,
-        destination: `${currentDir}\\${config.BuildDir}\\${modName}-${version}.zip`,
-        cwd: currentDir
+        source: "*",
+        destination: `${currentDir}\\${config.BuildDir}_zip\\${modName}-${version ?? ""}.zip`,
+        cwd: `${currentDir}/${config.BuildDir}`
     }).catch(function(err)
     {
         console.error(chalk.red("[SPT Build System] A bestzip error has occurred: ", err.stack));
     }).then(function()
     {
         // remove the temp directories
-        fs.rmSync(`${relativeZipPath}`, { force: true, recursive: true });
-        fs.rmSync(`${currentDir}/${modName}`, { force: true, recursive: true });
-        console.log(chalk.green(`[SPT Build System] Compressed mod package to: ${config.BuildDir}\\${modName}.zip`));
+        fs.rmSync(tempPath, { force: true, recursive: true });
+        console.log(chalk.green(`[SPT Build System] Compressed mod package to: ${config.BuildDir}_zip\\${modName}.zip`));
     });
 }
 
-if (config.StartServer === true && config.PathToRoot !== "")
+if (config.StartServer === true && config.PathToGame !== "")
 {
-    console.log(chalk.green(`[SPT Build System] Server starting in ${config.StartDelay} seconds`));
-    setTimeout(() => 
+    if (fs.existsSync(config.PathToGame))
     {
-        try 
+        console.log(chalk.green(`[SPT Build System] Server starting in ${config.StartDelay} seconds.`));
+        setTimeout(() =>
         {
-            const command = "Aki.Server.Exe";
-            const output = processHost.execSync(command, {
-                cwd: config.PathToRoot,
-                stdio: "inherit" // This allows the output to be displayed in the terminal
-            });
-            console.log("Command executed successfully:", output.toString());
-        }
-        catch (error) 
-        {
-            console.error("Error executing command:", error.message);
-        }
-    }, config.StartDelay * 1000);
+            try
+            {
+                const command = "Aki.Server.Exe";
+                const output = processHost.execSync(command, {
+                    cwd: config.PathToGame,
+                    stdio: "inherit" // This allows the output to be displayed in the terminal
+                });
+                console.log("Command executed successfully:", output.toString());
+            }
+            catch (error)
+            {
+                console.error("Error executing command:", error.message);
+            }
+        }, config.StartDelay * 1000);
+    }
+    else
+    {
+        console.log(chalk.red(`[SPT Build System] Server start failed, couldn't find your game folder at: "${config.PathToGame}".`));
+    }
 }
 else
 {
-    console.log(chalk.red("[SPT Build System] Error starting server: Is the server path configured?"));
+    if (config.StartServer === false)
+        console.log(chalk.yellow("[SPT Build System] Server start is Disabled in config."));
+    else if (config.PathToGame === "")
+        console.log(chalk.red("[SPT Build System] Server start was ignored because \"PathToGame\" in \"build.json\" isn't configured."));
 }
